bde6331f289404f45dcefca0b6e3af25263017ad
Timothy Ko <timothy.l.ko@gmail.com>
2018-03-31 02:51:48 -0500

    parsing script completed

--- backend/parseSheet.js
+++ backend/parseSheet.js
@@ -1,6 +1,6 @@
 var XLSX = require('xlsx')
 var mongoose = require('mongoose')
-var { Candidate } = require('./model')
+var Candidate = require('./model')
 
 mongoose.connect('mongodb://tko:tko@ds229549.mlab.com:29549/h4i-recruitment')
 mongoose.Promise = global.Promise
@@ -12,33 +12,34 @@ const wb = XLSX.readFile('candidates.xlsx')
 const ws = wb.Sheets[wb.SheetNames[0]]
 
 const jsonSheet = XLSX.utils.sheet_to_json(ws)
-var i = 0
-const candidate = jsonSheet[13]
-console.log('candidate: ', candidate);
-const newCandidate = new Candidate({
-  name: candidate.Name,
-  email: candidate.Email,
-  graduationDate: candidate['Graduation Date'],
-  major: candidate.Major,
-  minor: candidate.Minor,
-  resumeID: candidate.Resume,
-  github: candidate.Github,
-  linkedIn: candidate.LinkedIn,
-  website: candidate.Website,
-  role: candidate['Which role(s) are you applying for? '],
-  roleReason: candidate['For each role you have selected, please elaborate why you are applying for that role and why youwould be a good fit.'],
-  joinReason: candidate['Why do you want to join Hack4Impact, and what do you hope to gain from it?'],
-  timeCommitment: candidate['Please list your time commitments'],
-  timeCanDevote: candidate['8 hrs'],
-  techExperience: candidate['List technical/design experience (classes taken, side projects, internships, class projects, portfolio link)'],
-  howTheyKnowUs: candidate['How did you hear about us?'],
-  additionalComments: candidate['Any additional comments?'],
-  interviews: [{
-    timeCommitmentNotes: candidate['Time Commitment'],
-    dedicationToCommunityNotes: candidate['Dedication to Community'],
-    techCompetenceNotes: candidate['Technical Competence '],
-    otherNotes: candidate.Notes,
-    interviewer: candidate.Interviewers
-  }]
+
+jsonSheet.map(candidate => {
+  const newCandidate = new Candidate({
+    name: candidate.Name,
+    email: candidate.Email,
+    graduationDate: candidate['Graduation Date'],
+    major: candidate.Major,
+    minor: candidate.Minor,
+    resumeID: candidate.Resume,
+    github: candidate.Github,
+    linkedIn: candidate.LinkedIn,
+    website: candidate.Website,
+    role: candidate['Which role(s) are you applying for? '],
+    roleReason: candidate['For each role you have selected, please elaborate why you are applying for that role and why youwould be a good fit.'],
+    joinReason: candidate['Why do you want to join Hack4Impact, and what do you hope to gain from it?'],
+    timeCommitment: candidate['Please list your time commitments'],
+    timeCanDevote: candidate['8 hrs'],
+    techExperience: candidate['List technical/design experience (classes taken, side projects, internships, class projects, portfolio link)'],
+    howTheyKnowUs: candidate['How did you hear about us?'],
+    additionalComments: candidate['Any additional comments?'],
+    interviews: [{
+      timeCommitmentNotes: candidate['Time Commitment'],
+      dedicationToCommunityNotes: candidate['Dedication to Community'],
+      techCompetenceNotes: candidate['Technical Competence '],
+      otherNotes: candidate.Notes,
+      interviewer: candidate.Interviewers
+    }]
+  })
+  newCandidate.save()
 })
-newCandidate.save()
+console.log('Done Parsing & adding to Database... Ctrl-C to Quit')
