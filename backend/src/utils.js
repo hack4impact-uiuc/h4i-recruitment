92963f7101a3076b0a9302479cac1c8500c65512
Timothy Ko <timothy.l.ko@gmail.com>
2018-06-25 21:14:28 -0700

    #39 error Handling (#41)
    * fixes #39 Error Handling

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -1,13 +1,4 @@
 const { Stats } = require('./models')
-/*
- * Middleware to allow us to safely handle erros in async/await code
- * without wrapping each route in try...catch blocks
- */
-const errorWrap = fn => {
-  return (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next)
-  }
-}
 
 const getStats = async () => {
   const res = await Stats.find()
@@ -20,6 +11,5 @@ const getStats = async () => {
   return newStats
 }
 module.exports = {
-  errorWrap,
   getStats
 }
