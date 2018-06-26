e484050efcdb9e0f56aa3a36702c5432da22f101
shreyshrey1 <talk2shreyas@gmail.com>
2018-06-26 14:29:19 -0500

    feat: search bar works, it's ugly tho

--- frontend/src/components/candidateList.js
+++ frontend/src/components/candidateList.js
@@ -1,4 +1,5 @@
 // @flow
+import React, { Component } from 'react'
 import CandidateCardComponent from './candidateCard'
 import { Col } from 'reactstrap'
 
@@ -14,16 +15,41 @@ const CardCol = ({ children, ...rest }) => (
 
 // component that destructs Props - Props would look like this { candidates: {} }
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-const CandidateListComponent = ({ candidates }: Props) => (
-  <div className="candidate-list-box row">
-    {candidates !== undefined
-      ? candidates.map(candidate => (
-          <CardCol key={candidate._id}>
-            <CandidateCardComponent candidate={candidate} />
-          </CardCol>
-        ))
-      : 'No candidates'}
-  </div>
-)
+class CandidateListComponent extends Component {
+  constructor() {
+    super()
+    this.state = {
+      search: ''
+    }
+  }
+  handleChange = event => {
+    this.setState({ [event.target.id]: event.target.value })
+  }
+  render() {
+    const { search } = this.state
+    const { candidates } = this.props
+    return (
+      <div className="candidate-list-box row">
+        <form onSubmit={this.handleSubmit}>
+          <div className="mdl-textfield mdl-js-textfield">
+            <label htmlFor="team" className="mdl-textfield__label">
+              Search Candidates:
+            </label>
+            <input type="text" id="search" value={search} onChange={this.handleChange} />
+          </div>
+        </form>
+        {candidates !== undefined
+          ? candidates.map(candidate => {
+              return candidate.name.toLowerCase().includes(this.state.search.toLowerCase()) ? (
+                <CardCol key={candidate._id}>
+                  <CandidateCardComponent candidate={candidate} />
+                </CardCol>
+              ) : null
+            })
+          : 'No candidates'}
+      </div>
+    )
+  }
+}
 
 export default CandidateListComponent
