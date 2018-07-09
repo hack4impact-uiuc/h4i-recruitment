549f070f2a4ecc94efeb39696f8ee15c138ef08b
Hana Rimawi <hrimaw2@illinois.edu>
2018-07-08 22:14:32 -0500

    Started interview page

---
+++ frontend/src/pages/interview.js
@@ -0,0 +1,41 @@
+import { Component } from 'react'
+import Head from '../components/head'
+import Nav from '../components/nav'
+
+type Props = {}
+class Interview extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: '' }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value })
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value)
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+      <div>
+        <Head title="Interview" />
+        <Nav />
+        <h1>Interview Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
+  }
+}
+export default Interview
