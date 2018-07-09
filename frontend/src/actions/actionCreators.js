30925cc021c2ea0908e677f2399e1665bf23fff1
Roy Chiu <rychiu13@gmail.com>
2018-07-09 15:04:05 -0500

    Specify imports

--- frontend/src/actions/actionCreators.js
+++ frontend/src/actions/actionCreators.js
@@ -1,11 +1,11 @@
-import * as action from './actionTypes.js'
+import NEW_MATCH from './actionTypes.js'
 
 // Action Creators
 
 // Function to create action that stores data about new match
 function newMatch(id_1, id_2) {
   return {
-    type: action.NEW_MATCH,
+    type: NEW_MATCH,
     candidates: [id_1, id_2]
   }
 }
