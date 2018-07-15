fb44a29c7517418531bd00704fbc331d7b92c116
shreyshrey1 <talk2shreyas@gmail.com>
2018-07-15 15:22:38 -0500

    fix: fixed tests

--- backend/tests/getGithubContributions.test.js
+++ backend/tests/getGithubContributions.test.js
@@ -7,10 +7,10 @@ const {getGithubContributions} = require('../src/utils/gitScraper')
 
 describe ('getGithubContributions', () => {
     it ('should return a string', async () => {
-      assert.typeOf(getGithubContributions("https://github.com/shreyshrey1"), 'string');
-      assert.typeOf(getGithubContributions("https://github.com/tko22"), 'string');
-      assert.typeOf(getGithubContributions("https://github.com/angadgarg25"), 'string');
-      assert.typeOf(getGithubContributions("https://github.com/rgychiu"), 'string');
-      assert.typeOf(getGithubContributions("https://github.com/CodeBrew28"), 'string');
+      assert.typeOf(await getGithubContributions("https://github.com/shreyshrey1"), 'string');
+      assert.typeOf(await getGithubContributions("https://github.com/tko22"), 'string');
+      assert.typeOf(await getGithubContributions("https://github.com/angadgarg25"), 'string');
+      assert.typeOf(await getGithubContributions("https://github.com/rgychiu"), 'string');
+      assert.typeOf(await getGithubContributions("https://github.com/CodeBrew28"), 'string');
     })
   })
