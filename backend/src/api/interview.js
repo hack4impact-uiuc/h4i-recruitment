956092c0dd3b7dc2f56700d4090d35260a60fcac
Hana Rimawi <hrimaw2@illinois.edu>
2018-07-16 21:46:30 -0500

    my first express request lol

--- backend/src/api/interview.js
+++ backend/src/api/interview.js
@@ -20,7 +20,31 @@ router.get(
         res.json({ interview: retInterview })
     })
 )
-  
+
+router.post(
+    '/add-interview',
+    errorWrap(async (req, res) => {
+      data = req.body
+      let response = 'Interview Added Sucessfully'
+      let interviewerIds = data.interviewers;
+      let candidateId = data.candidateId;
+      let reqSections = data.sections;
+
+      if (interviewerIds == undefined | interviewerIds == undefined | interviewerIds == undefined) {
+        response = 'Invalid request'
+      }
+      else {
+        await Candidate.findByIdAndUpdate(candidateId, { status: 'interviewing' })
+        const interview = new Interview({
+            interviewers: interviewerIds,
+            candidate: candidateId,
+            sections: reqSections
+        })
+        await interview.save()
+      }
+      res.json({ result: response })
+    })
+  )
 
 
 module.exports = router
