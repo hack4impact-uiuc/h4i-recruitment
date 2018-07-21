77b0a51ae992f37374fe5e6a8bbcdc1f3ac6d70b
Hana Rimawi <hrimaw2@illinois.edu>
2018-07-21 00:16:17 -0500

    added more requests to Interview model

--- backend/src/api/interview.js
+++ backend/src/api/interview.js
@@ -27,18 +27,22 @@ router.post(
       data = req.body
       let response = 'Interview Added Sucessfully'
       let interviewerIds = data.interviewers;
-      let candidateId = data.candidateId;
+      let interviewerKey = data.interviewerKey;
       let reqSections = data.sections;
+      let candidateId = data.candidateId;
+      let score = data.overallScore;
 
-      if (interviewerIds == undefined | interviewerIds == undefined | interviewerIds == undefined) {
-        response = 'Invalid request'
+      if (interviewerIds == undefined | candidateId == undefined | reqSections == undefined) {
+        response = 'Invalid Add Interview request'
       }
       else {
         await Candidate.findByIdAndUpdate(candidateId, { status: 'interviewing' })
         const interview = new Interview({
+            interviewerKey: interviewerKey,
+            sections: reqSections,
+            overallScore: score,
             interviewers: interviewerIds,
-            candidate: candidateId,
-            sections: reqSections
+            candidateId: candidateId
         })
         await interview.save()
       }
@@ -46,5 +50,38 @@ router.post(
     })
   )
 
+  router.delete(
+    '/delete-interview',
+    errorWrap(async (req, res) => {
+      data = req.body
+      let response = 'Interview Deleted Sucessfully'
+      let interviewId = data.id
+
+      if (interviewId == undefined) {
+        response = 'Invalid Delete Interview request'
+      }
+      else {
+        Interview.remove({ id: interviewId })
+      }
+      res.json({ result: response })
+    })
+  )
+
+router.put(
+    '/edit-interview',
+    errorWrap(async (req, res) => {
+      data = req.body
+      let response = 'Interview Edited Sucessfully'
+      let interviewId = data.id
+      let reqSections = data.sections
+      if (interviewId == undefined) {
+        response = 'Invalid Edit Interview request'
+      }
+      else {
+        Interview.findByIdAndUpdate({ sections: reqSections })
+      }
+      res.json({ result: response })
+    })
+  )
 
 module.exports = router
