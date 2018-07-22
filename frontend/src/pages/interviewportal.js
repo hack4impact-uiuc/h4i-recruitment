e32b11235006f16fbaa4ea052e0fec4c3bf49032
Megha Mallya <meghamallya1@gmail.com>
2018-07-21 17:47:39 -0700

    added an interview portal and converted modal to use reactstrap

---
+++ frontend/src/pages/interviewportal.js
@@ -0,0 +1,28 @@
+import { Button, Container} from 'reactstrap'
+import { Component } from 'react'
+import Link from 'next/link'
+import Head from '../components/head'
+import Nav from '../components/nav'
+
+type Props = {}
+
+// Main app
+class Login extends Component<Props> {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+        <Container>
+            <Head title="Interview Portal" />
+            <Nav />
+            <Link prefetch href="/interview">
+                <Button color="primary">New Interview</Button>
+            </Link>
+            {' '}
+            <Button color="primary">Edit Interview</Button>
+        </Container>
+    )
+  }
+}
+export default Login
