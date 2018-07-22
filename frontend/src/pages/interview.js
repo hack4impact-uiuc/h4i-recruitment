eea24d4514f9f4cea0c0fa1da781cea76bfc8b44
Megha Mallya <meghamallya1@gmail.com>
2018-07-21 22:39:51 -0700

    added draft of interview page

--- frontend/src/pages/interview.js
+++ frontend/src/pages/interview.js
@@ -1,21 +1,16 @@
 import { Component } from 'react'
+import { Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
 import Head from '../components/head'
 import Nav from '../components/nav'
+import Link from 'next/link'
 
 type Props = {}
-class Interview extends React.Component {
+class Interview extends Component<Props> {
   constructor(props) {
     super(props)
     this.state = { value: '' }
-
-    this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value })
-  }
-
   handleSubmit(event) {
     alert('A name was submitted: ' + this.state.value)
     event.preventDefault()
@@ -23,18 +18,163 @@ class Interview extends React.Component {
 
   render() {
     return (
-      <div>
+      <Container>
         <Head title="Interview" />
         <Nav />
-        <h1>Interview Form</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
+        <Form>
+          <FormGroup>
+            <legend>Time</legend>
+            <FormGroup check>
+              <span>
+                <Input type="checkbox" name="time" />{' '}
+                Exec member for another org
+              </span>
+            </FormGroup>
+            <FormGroup check>
+              <span>
+                <Input type="checkbox" name="time" />{' '}
+                Consulting club such as IBC, OTCR
+              </span>
+            </FormGroup>
+            <FormGroup check>
+              <span >
+                <Input type="checkbox" name="time"/>{' '}
+                Some other club that requires time (ex: Enactus, Fraternity Pledge during the same semester)
+              </span>
+            </FormGroup>
+            <FormGroup check>
+              <span >
+                <Input type="checkbox" name="time"/>{' '}
+                Hard/Time-consuming classes
+              </span>
+            </FormGroup>
+            <FormGroup check>
+              <span >
+                <Input type="checkbox" name="time"/>{' '}
+                Hard course-load (still take one point off for each hard class they have as described above)
+              </span>
+            </FormGroup>
+            <FormGroup check>
+              <span >
+                <Input type="checkbox" name="time"/>{' '}
+                Too many org obligations
+              </span>
+            </FormGroup>
+          </FormGroup>
+          <FormGroup>
+            <legend>Initiative and Passion</legend>
+            <FormGroup check>
+              <span>
+                <Input type="radio" name="passion" />{' '}
+                0 - Definitely using this as a resume booster
+              </span>
+            </FormGroup>
+            <FormGroup check>
+              <span>
+                <Input type="radio" name="passion" />{' '}
+                1 - Had a general idea of what hack4impact was. They probably only read a couple sentences about Hack4Impact and applied and that's it.
+              </span>
+            </FormGroup>
+            <FormGroup check>
+              <span >
+                <Input type="radio" name="passion" />{' '}
+                2 - Really into it, thinking about ways they could contribute to the org
+              </span>
+            </FormGroup>
+          </FormGroup>
+          <FormGroup>
+            <legend>Community</legend>
+            <span>Will they contribute to community or are they just using this as a resume booster? </span>
+            <Input type="text" placeholder="Your Answer" />
+          </FormGroup>
+          <FormGroup>
+            <legend>Resume and Tech Knowledge</legend>
+            <FormGroup check>
+              <span>
+                <Input type="radio" name="techknowledge" />{' '}
+                0 - No Experience
+              </span>
+            </FormGroup>
+            <FormGroup check>
+              <span>
+                <Input type="radio" name="techknowledge" />{' '}
+                1 - 1 to 2 projects
+              </span>
+            </FormGroup>
+            <FormGroup check>
+              <span >
+                <Input type="radio" name="techknowledge" />{' '}
+                2 - Has internship or a couple substantial projects
+              </span>
+            </FormGroup>
+            <FormGroup check>
+              <span >
+                <Input type="radio" name="techknowledge"/>{' '}
+                3 - Multiple
+              </span>
+            </FormGroup>
+          </FormGroup>
+          <FormGroup>
+            <legend>Knowledge of Web Dev</legend>
+            <FormGroup check>
+              <span>
+                <Input type="radio" name="webdev" />{' '}
+                0 - No experience
+              </span>
+            </FormGroup>
+            <FormGroup check>
+              <span>
+                <Input type="radio" name="webdev" />{' '}
+                1 - Some experience with it
+              </span>
+            </FormGroup>
+            <FormGroup check>
+              <span >
+                <Input type="radio" name="webdev"/>{' '}
+                2 - Has done a couple projects, knows what flask is. etc. 
+              </span>
+            </FormGroup>
+            <FormGroup check>
+              <span >
+                <Input type="radio" name="webdev" disabled />{' '}
+                3 - Knows more than you - if they are a you think they could be a tech lead
+              </span>
+            </FormGroup>
+          </FormGroup>
+          <FormGroup>
+            <legend>Technical Challenge</legend>
+            <FormGroup check>
+              <span>
+                <Input type="radio" name="techchallenge" />{' '}
+                0 - No experience
+              </span>
+            </FormGroup>
+            <FormGroup check>
+              <span>
+                <Input type="radio" name="techchallenge" />{' '}
+                1 - Completed but with a lot of help/slow
+              </span>
+            </FormGroup>
+            <FormGroup check>
+              <span >
+                <Input type="radio" name="techchallenge" disabled />{' '}
+                2 - Completed in a reasonable amount of time
+              </span>
+            </FormGroup>
+            <FormGroup check>
+              <span >
+                <Input type="radio" name="techchallenge" disabled />{' '}
+                3 - Damn they are good
+              </span>
+            </FormGroup>
+          </FormGroup>
+          <FormGroup>
+            <Link prefetch href="/interviewportal">
+              <Button color="primary">Submit</Button>
+            </Link>
+          </FormGroup>
+        </Form>
+      </Container>
     )
   }
 }
--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,114 +1,38 @@
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
-// const ReactCSSTG = React.addons.CSSTransitionGroup
+import { Modal, Container, Button, Input, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 import { Component } from 'react'
+import Link from 'next/link'
 import Head from '../components/head'
 import Nav from '../components/nav'
 
 type Props = {}
 
 // Main app
-class Login extends React.Component {
+class Login extends Component<Props> {
   constructor(props) {
     super(props)
-    this.state = {
-      isVisible: true
-    }
-    // Bindings
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleRemount = this.handleRemount.bind(this)
-  }
-
-  handleSubmit(e) {
-    e.preventDefault()
-    this.setState(
-      {
-        isVisible: false
-      },
-      function() {
-        console.log(this.state.isVisible)
-      }
-    )
-    return false
-  }
-  handleRemount(e) {
-    this.setState(
-      {
-        isVisible: true
-      },
-      function() {
-        console.log(this.state.isVisible)
-      }
-    )
-    e.preventDefault()
   }
   render() {
-    // const for React CSS transition declaration
-    let component = this.state.isVisible ? (
-      <Modal onSubmit={this.handleSubmit} key="modal" />
-    ) : (
-      <ModalBack onClick={this.handleRemount} key="bringitback" />
-    )
-
     return (
-      <div>
+      <Container>
         <Head title="Login" />
         <Nav />
-        <h2>Enter your key</h2>
-        <ReactCSSTransitionGroup
-          transitionName="animation"
-          transitionAppear={true}
-          transitionAppearTimeout={500}
-          transitionEnterTimeout={500}
-          transitionLeaveTimeout={300}
-        >
-          {component}
-        </ReactCSSTransitionGroup>
-      </div>
+        <Modal isOpen={true} >
+          <ModalHeader >Login to Your Interview Portal</ModalHeader>
+          <ModalBody>
+            <Input type="text" name="Input Key" placeholder="Input Your Key"/>  
+          </ModalBody>
+          <ModalFooter>
+            <Link prefetch href="/">
+              <Button color="secondary">Cancel</Button>
+            </Link>
+            {' '}
+            <Link prefetch href="/interviewportal">
+              <Button color="primary">Submit</Button>
+            </Link>
+          </ModalFooter>
+        </Modal>
+      </Container>
     )
   }
 }
-
-// Modal
-class Modal extends React.Component {
-  render() {
-    return (
-      <div className="Modal">
-        <form onSubmit={this.props.onSubmit}>
-          <Input type="text" name="key" placeholder="key" />
-          <button> Sign In</button>
-        </form>
-        <a href="#">Lost your key ?</a>
-      </div>
-    )
-  }
-}
-
-// Generic input field
-class Input extends React.Component {
-  render() {
-    return (
-      <div className="Input">
-        <input
-          type={this.props.type}
-          name={this.props.name}
-          placeholder={this.props.placeholder}
-          required
-          autocomplete="false"
-        />
-      </div>
-    )
-  }
-}
-
-// Button to brind the modal back
-class ModalBack extends React.Component {
-  render() {
-    return (
-      <button className="bringitback" onClick={this.props.onClick} key={this.props.className}>
-        Brind the modal back !
-      </button>
-    )
-  }
-}
-
 export default Login
