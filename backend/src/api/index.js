3744e4df6a6b4cbc28e62b87fc767e02a0b6f31c
Hana Rimawi <hrimaw2@illinois.edu>
2018-07-26 21:26:58 -0500

    Thanku megha

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,9 +1,11 @@
 const candidates = require('./candidates')
+const interview = require('./interview')
 const matchCandidates = require('./matchCandidates')
 const matches = require('./matches')
 
 module.exports = {
   candidates,
+  interview,
   matchCandidates,
   matches
 }
