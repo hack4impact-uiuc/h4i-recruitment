4030d1112bdf32298594b09ed2a1cbbe62e2edc9
CodeBrew28 <arshiam2@illinois.edu>
2018-07-29 18:07:07 -0700

    deploying the backend, not donte with frontend yet

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ const app = require('./app')
 const mongoose = require('mongoose')
 
 // connect to mongoose
+console.log(process.env)
 mongoose.connect(process.env.MONGO_URL)
 mongoose.Promise = global.Promise
 mongoose.connection
@@ -13,6 +14,6 @@ app.listen(8080, async () => console.log('Server listening on port 8080!'))
 
 process.on('unhandledRejection', error => {
   // Will print "unhandledRejection err is not defined"
-  console.error('ERROR: unhandledRejection, did you run `dotenv` before yarn dev?', error.message)
+  // console.error('ERROR: unhandledRejection, did you run `dotenv` before yarn dev?', error.message)
   process.exit(1)
 })
