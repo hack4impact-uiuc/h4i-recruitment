f80cbc4e5c8f4c8b6fe6ee3d215c7fbd54a12ded
Angad Garg <angad.garg3@gmail.com>
2018-08-04 20:16:54 -0500

    started work

--- backend/src/api/matchCandidates.js
+++ backend/src/api/matchCandidates.js
@@ -8,19 +8,56 @@ router.get(
   errorWrap(async (req, res) => {
     // not very efficient, nor should we have this algorithm for matches
     // TODO: change this to a better algorithm
-    const candidates = await Candidate.aggregate([{ $sample: { size: 5 } }])
+    const min = await Candidate.aggregate([{ $group: {
+      _id: {},
+      min: { $min: "$facemashRankings.numOfMatches" }
+    }}]).min
+
+    id1 = await Candidate.aggregate([
+      { $match: { "facemashRankings.numOfMatches": min } },
+      { $sample: { size: 1 } }
+    ]).[0]_id
+    if (min_candidates.length() === 1) {
+      id1 = min_candidates[0]._id
+      const secondMin = await Candidate.aggregate([
+        { $match: { _id: { $ne: id1 } } }
+        { $group: {
+          _id: {},
+          min: { $min: "$facemashRankings.numOfMatches" }
+        }}
+      ]).min
+
+      id2 = await Candidate.aggregate([
+        { $match: { "facemashRankings.numOfMatches": secondMin } },
+        { $sample: { size: 1 } }
+      ])[0]._id
+    } else {
+      id1 = min_candidates[0]._id
+      id2 = min_candidates[1]._id
+    }
     // create the match
     const match = new Match({
-      candidate1: candidates[0]._id,
-      candidate2: candidates[1]._id
+      candidate1: id1,
+      candidate2: id2
     })
     await match.save()
+
+    const candidate1 = await Candidate.findById(id1)
+    candidate1.facemashRankings.numOfMatches += 1
+    candidate1.save()
+
+    const candidate2 = await Candidate.findById(id2)
+    candidate2.facemashRankings.numOfMatches += 1
+    candidate2.save()
     res.json({
+      code: 200,
+      message: '',
       result: {
-        candidate1: candidates[0],
-        candidate2: candidates[1],
+        candidate1: candidate1,
+        candidate2: candidate2,
         matchID: match._id
-      }
+      },
+      success: 'true'
     })
   })
 )
