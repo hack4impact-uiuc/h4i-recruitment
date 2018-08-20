2f6adb8fd0a424473d1b9ce9a99ff38f38bafbe9
Timothy Ko <timothy.l.ko@gmail.com>
2018-08-19 20:18:37 -0700

    auth middleware passes key and key's corresponding name through the request object

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -6,9 +6,25 @@ const auth = (req, res, next) => {
   const key = req.query.key
   if (key != undefined) {
     if (key && key.length === 11) {
-      keyVerified = keyData.keys.filter(currKey => currKey.key === key).length !== 0
+      const keysFiltered = keyData.keys.filter(currKey => currKey.key === key)
+      keyVerified = keysFiltered.length !== 0
 
       if (keyVerified) {
+        req._key_name = keysFiltered[0].name // set the user's name of the key that was used to make the request
+        req._key = key
+
+        // check if there is a corresponding name for the key
+        if (req._key_name == undefined) {
+          msg = `The key '${key}'given doesn't have a corresponding name. Check the json file holding the keys.`
+          console.error(msg)
+          res.status(500).json({
+            code: 500,
+            message: 'Bad Key',
+            success: false
+          })
+        }
+
+        // check whether key is a lead's key or a member's key
         if (key.endsWith(process.env.LEAD_SUFFIX)) {
           req._is_lead = true
         } else {
