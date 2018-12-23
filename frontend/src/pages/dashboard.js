efa35e0e1c1c69cd84d6520661e2ed397584a8e3
Timothy Ko <timothy.l.ko@gmail.com>
2018-12-22 23:03:30 -0800

    fix

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -92,8 +92,7 @@ class Dashboard extends React.Component<Props> {
       .filter(x => this.state.filters.statuses.includes(x.status))
       .filter(x => this.state.filters.years.includes(x.year))
       .filter(x => !x.role.map(role => this.state.filters.roles.includes(role)).includes(false))
-    console.log(this.state.filters.roles)
-    // .filter(x => x.name.toLowerCase().includes(this.state.search.toLowerCase()))
+      .filter(x => x.name.toLowerCase().includes(this.state.search.toLowerCase()))
     switch (this.state.filters.sortBy[0]) {
       case 'Name':
         filteredCandidates = filteredCandidates.sort(sortByProperty('name'))
