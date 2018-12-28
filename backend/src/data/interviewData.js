b5ad73afd2d6a4f58f5d7e0d26779a0888badaef
Arpan Laha <arpanlaha99@gmail.com>
2018-12-28 00:37:16 -0600

    format

--- backend/src/data/interviewData.js
+++ backend/src/data/interviewData.js
@@ -1,13 +1,19 @@
 export default [
-    {
-        round: "1",
-        type: "mass_interview",
-        categories: [
-            {
-                title: "Overall Score",
-                minScore: 0,
-                maxScore: 5,
-                description: <p style={{ color: "grey" }}>description here which would show up as grey. Note that you could make this an actual react component here</p>
-            }
-        ]
+  {
+    round: '1',
+    type: 'mass_interview',
+    categories: [
+      {
+        title: 'Overall Score',
+        minScore: 0,
+        maxScore: 5,
+        description: (
+          <p style={{ color: 'grey' }}>
+            description here which would show up as grey. Note that you could make this an actual
+            react component here
+          </p>
+        )
+      }
+    ]
+  }
 ]
