a34189a074ad7d78dc3a180051f9cc545acd2d37
Angad Garg <angad.garg3@gmail.com>
2019-06-18 23:00:05 -0700

    Fix mongo attributes to be actually required

--- backend/src/models/match.js
+++ backend/src/models/match.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const Matches = new mongoose.Schema({
   candidate1: { type: mongoose.Schema.Types.ObjectId, required: true },
-  candidate2: { type: mongoose.Schema.Types.ObjectId, require: true },
+  candidate2: { type: mongoose.Schema.Types.ObjectId, required: true },
   winnerID: { type: mongoose.Schema.Types.ObjectId, default: null },
   submittedBy: { type: String, default: '' },
   submittedByKey: { type: String, default: '' },
