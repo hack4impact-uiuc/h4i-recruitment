b436a7d1e8cee153389ae836bb3b2655582cf74d
kelley <kelleyc2@illinois.edu>
2019-07-06 21:09:04 -0500

    Formatting

--- backend/src/models/cycle.js
+++ backend/src/models/cycle.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 /**
  * Holds information about a recruitment cycle; attributes are subject to change.
- * 
+ *
  * term: (for example) SP19, FA20
  * chapter: (or school) UIUC, GaTech
  */
