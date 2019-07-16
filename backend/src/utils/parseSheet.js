c28b00aeb15ed9dfcb09fd394f6a8251816c25d5
Kelley Chau <kelley@runoranj.com>
2019-07-16 14:35:50 -0500

    Not something I should be commiting but I don't bring my work computer home

--- backend/src/utils/parseSheet.js
+++ backend/src/utils/parseSheet.js
@@ -16,8 +16,19 @@ mongoose.connection
 
 const wb = XLSX.readFile(__dirname + '/candidates.xlsx')
 const ws = wb.Sheets[wb.SheetNames[0]]
-
 const jsonSheet = XLSX.utils.sheet_to_json(ws)
+
+const gradToYear = {
+  'Fall 2019': yearsEnum.SENIOR,
+  'Spring 2020': yearsEnum.SENIOR,
+  'Fall 2020': yearsEnum.JUNIOR,
+  'Spring 2021': yearsEnum.JUNIOR,
+  'Fall 2021': yearsEnum.SOPHOMORE,
+  'Spring 2022': yearsEnum.SOPHOMORE,
+  'Fall 2022': yearsEnum.SOPHOMORE,
+  'Spring 2023': yearsEnum.FRESHMAN
+}
+
 function sleep(ms) {
   return new Promise(resolve => {
     setTimeout(resolve, ms)
@@ -28,80 +39,75 @@ async function parseStuff() {
   await sleep(3000)
   for (let i = 0; i < jsonSheet.length; i++) {
     let candidate = jsonSheet[i]
-    console.log(candidate.Name)
-    let githubContributions = 'N/A'
-    if (candidate['Github Link']) {
-      githubContributions = await getGithubContributions(candidate['Github Link'])
-    }
-    let year = null
-    switch (candidate['Graduation Date']) {
-      case gradEnum.SP19:
-        year = yearsEnum.SENIOR
-        break
-      case gradEnum.FA19:
-        year = yearsEnum.JUNIOR
-        break
-      case gradEnum.SP20:
-        year = yearsEnum.JUNIOR
-        break
-      case gradEnum.FA20:
-        year = yearsEnum.SOPHOMORE
-        break
-      case gradEnum.SP21:
-        year = yearsEnum.SOPHOMORE
-        break
-      case gradEnum.FA21:
-        year = yearsEnum.SOPHOMORE
-        break
-      case gradEnum.SP22:
-        year = yearsEnum.FRESHMAN
-        break
-    }
+    // console.log(candidate.Name)
+    // let githubContributions = 'N/A'
+    // if (candidate['Github Link']) {
+    //   githubContributions = await getGithubContributions(candidate['Github Link'])
+    // }
+    // let year = gradToYear[candidate['Graduation Date']]
+    let keys = Object.keys(candidate)
+    console.log(keys) // making sure keys works
+    console.log(year) // making sure the enum works
+
+    // Might throw error here?
+    // idk if this one even works
+
+    // get all the questions and then make an input name them
+    // then create a string to string map and make those columns in the candidate field
+    //
     if (year == null) {
       console.log(`WEIRD GRAD DATE ${candidate.Name}`)
     }
-    let newCandidate = new Candidate({
-      name: candidate.Name,
-      email: candidate['Email Address'],
-      graduationDate: candidate['Graduation Date'],
-      status: statusEnum.PENDING,
-      major: candidate.Major,
-      minor: candidate['Minor(s)'],
-      resumeID: candidate.Resume,
-      github: candidate['Github Link'],
-      linkedIn: candidate.LinkedIn,
-      website: candidate.Website,
-      role: candidate['Which role(s) are you applying for? '].split(', '),
-      githubContributions: githubContributions,
-      year: year,
-      classesTaken:
-        candidate['Which classes have you taken?'] != undefined
-          ? candidate['Which classes have you taken?'].split(', ')
-          : [],
-      roleReason:
-        candidate[
-          'For each role you have selected, please elaborate why you are applying for that role and why you would be a good fit.'
-        ],
-      joinReason:
-        candidate['Why do you want to join Hack4Impact, and what do you hope to gain from it?'],
-      timeCommitment: candidate['Please list your time commitments'],
-      techExperience:
-        candidate[
-          'List technical/design experience (side projects, internships, class projects, portfolio link, additional classes)'
-        ],
-      howTheyKnowUs: candidate['How did you hear about us?'],
-      additionalComments: candidate['What else should we know about you?'],
-      interviews: [
-        {
-          timeCommitmentNotes: candidate['Please list your time commitments'],
-          dedicationToCommunityNotes: candidate['Dedication to Community'],
-          techCompetenceNotes: candidate['Technical Competence '],
-          otherNotes: candidate.Notes,
-          interviewer: candidate.Interviewers
-        }
-      ]
-    })
+    /* Refactor ideas: make an obj to pass into the Candidate constructor
+     * Make a map from the field name to the excel column
+     * - Make sure to use [""] for each field name
+     * And then you can add the extra keys on top
+     * Loop through and add them all unless there's specifics for them, which we can't support dynamics for
+     */
+
+    // let newCandidate = new Candidate({
+    //   name: candidate.Name,
+    //   email: candidate['Email Address'],
+    //   graduationDate: candidate['Graduation Date'],
+    //   status: statusEnum.PENDING,
+    //   major: candidate.Major,
+    //   minor: candidate['Minor(s)'],
+    //   resumeID: candidate.Resume,
+    //   github: candidate['Github Link'],
+    //   linkedIn: candidate.LinkedIn,
+    //   website: candidate.Website,
+    //   role: candidate['Which role(s) are you applying for? '].split(', '),
+    //   githubContributions: githubContributions,
+    //   year: year,
+    //   classesTaken:
+    //     candidate['Which classes have you taken?'] != undefined
+    //       ? candidate['Which classes have you taken?'].split(', ')
+    //       : [],
+    //   roleReason:
+    //     candidate[
+    //       'For each role you have selected, please elaborate why you are applying for that role and why you would be a good fit.'
+    //     ],
+    //   joinReason:
+    //     candidate['Why do you want to join Hack4Impact, and what do you hope to gain from it?'],
+    //   timeCommitment: candidate['Please list your time commitments'],
+    //   techExperience:
+    //     candidate[
+    //       'List technical/design experience (side projects, internships, class projects, portfolio link, additional classes)'
+    //     ],
+    //   howTheyKnowUs: candidate['How did you hear about us?'],
+    //   additionalComments: candidate['What else should we know about you?'],
+    //   interviews: [
+    //     {
+    //       timeCommitmentNotes: candidate['Please list your time commitments'],
+    //       dedicationToCommunityNotes: candidate['Dedication to Community'],
+    //       techCompetenceNotes: candidate['Technical Competence '],
+    //       otherNotes: candidate.Notes,
+    //       interviewer: candidate.Interviewers
+    //     }
+    //   ]
+    // })
     // save errors out if candidate doesn't match the schema
+    // Make this a promise? I really don't like any of the manual sleep stuff going on
     const res = await newCandidate.save()
     console.log(res)
     await sleep(500)
