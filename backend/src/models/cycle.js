c2af0c534cc2edd61426d9ff22997e60c3d2bec6
alan-fang <fang.alan.7@gmail.com>
2019-07-15 22:29:46 -0700

    yarn format

--- backend/src/models/cycle.js
+++ backend/src/models/cycle.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
  * Holds information about a recruitment cycle; attributes are subject to change.
  *
  * term: (for example) SP19, FA20
- * workspaceName: full name of the workspace that a cycle is associated with (Georgia Tech Hack4Impact and 
+ * workspaceName: full name of the workspace that a cycle is associated with (Georgia Tech Hack4Impact and
  * not GaTech H4I). This is a strong suggestion but not enforced!
  */
 
