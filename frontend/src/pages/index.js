3d0f07aa589a114c48df253eb303d61337c77403
Betty Shen <cs2@illinois.edu>
2019-07-19 14:04:29 -0400

    move changes to index.js

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -24,76 +24,6 @@ import {
 type Props = {}
 
 class LoginPage extends React.Component<Props> {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     currentKey: '',
-  //     loading: false
-  //   }
-  // }
-  // handleSubmit = async () => {
-  //   this.setState({
-  //     loading: true
-  //   })
-  //   const { success, result } = await validateKey(this.state.currentKey)
-  //   if (success) {
-  //     localStorage.setItem('interviewerKey', this.state.currentKey)
-  //     localStorage.setItem('interviewerName', result.name)
-  //     this.setState({
-  //       loading: false
-  //     })
-  //     Router.push('/dashboard')
-  //   } else {
-  //     this.setState({
-  //       loading: false
-  //     })
-  //     alert("Couldn't log in. Is your key correct?")
-  //   }
-  // }
-  // onTextChange = e => {
-  //   this.setState({ currentKey: e.target.value })
-  // }
-  // // handles when user presses "Enter" when input is focused
-  // _handleKeyPress = e => {
-  //   if (e.key === 'Enter') {
-  //     this.handleSubmit()
-  //   }
-  // }
-  // render() {
-  //   return (
-  //     <>
-  //       <Head />
-  //       <Nav />
-  //       <Container>
-  //         {this.state.loading ? (
-  //           <Row className="login-loading-box">
-  //             <Col md="4" />
-  //             <Col md="2">
-  //               <ReactLoading className="login-box" type="spinningBubbles" color="#000" />
-  //             </Col>
-  //           </Row>
-  //         ) : (
-  //           <div className="align-middle login-box">
-  //             <h4>Enter Key:</h4>
-  //             <Input
-  //               autoFocus={true}
-  //               type="text"
-  //               value={this.state.currentKey}
-  //               onChange={this.onTextChange}
-  //               name="Input Key"
-  //               placeholder="Input Your Key"
-  //               onKeyPress={this._handleKeyPress}
-  //             />
-  //             <Button className="mt-3" color="primary" onClick={this.handleSubmit}>
-  //               Login
-  //             </Button>
-  //           </div>
-  //         )}
-  //       </Container>
-  //     </>
-  //   )
-  // }
-
   constructor(props) {
     super(props)
     this.state = {
@@ -118,15 +48,7 @@ class LoginPage extends React.Component<Props> {
   }
 
   handleSubmit = async e => {
-    // e.preventDefault()
-    // const result = await login(this.state.email, this.state.password)
-    // const resp = await result.json()
-    // if (!resp.token) {
-    //   this.setState({ errorMessage: resp.message })
-    // } else {
-    //   setCookie('token', resp.token)
-    //   Router.push('/')
-    // }
+
   }
 
   render() {
