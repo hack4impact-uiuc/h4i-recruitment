c1e605b54dbc139872b2db557078ecc02d1aec32
Alice Fang <32147571+alicesf2@users.noreply.github.com>
2019-07-21 16:38:53 -0700

    Resolves #307 create separate modal component for use on event pages
    * created custom events modal component and reformatted event overview page to use it
    
    * addressed feedback, also fixed a lotta props stuff i messed up lolrip
    
    * removed unnecessary fragment
    
    * yarn format

---
+++ frontend/src/utils/formFields.js
@@ -0,0 +1,39 @@
+const newEventFields = [
+  {
+    label: 'Name',
+    name: 'name',
+    placeholder: 'i.e. Product Showcase'
+  },
+  {
+    label: 'Date',
+    name: 'date',
+    type: 'date'
+  },
+  {
+    label: 'Start Time',
+    name: 'startTime',
+    type: 'time'
+  },
+  {
+    label: 'End Time',
+    name: 'endTime',
+    type: 'time'
+  },
+  {
+    label: 'Location',
+    name: 'location',
+    placeholder: 'i.e. ECEB'
+  },
+  {
+    label: 'Description',
+    name: 'description',
+    placeholder: 'i.e. Teams present their final product.'
+  },
+  {
+    label: 'Facebook Event Link',
+    name: 'fbLink',
+    placeholder: 'i.e. https://www.facebook.com/events/2405129342892922/'
+  }
+]
+
+export { newEventFields }
