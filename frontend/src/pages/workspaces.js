da1ccad994f160225664e0d8aadabe5bb1cfb2b0
Kelley Chau <kelley@runoranj.com>
2019-07-30 10:19:03 -0500

    Adding temp display table for workspaces + cycles

--- frontend/src/pages/workspaces.js
+++ frontend/src/pages/workspaces.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Container, Table } from 'reactstrap'
 import Nav from '../components/nav'
 import Head from '../components/head'
-import { getCycles } from '../utils/api'
+import { getWorkspaces, createWorkspace, createCycle, getCyclesByWorkspace } from '../utils/api'
 import Router from 'next/router'
 import Link from 'next/link'
 
@@ -10,16 +10,24 @@ class Workspaces extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      cycles: []
+      workspaces: []
     }
   }
 
   async componentDidMount() {
     // get workspaces and cycle, if applicable
-    const res = await getCyclesByWorkspace('UIUC')
-    this.setState({
-      cycles: res && res.result
-    })
+    const workspaceRes = await getWorkspaces()
+    if (workspaceRes) {
+      workspaceRes.result.forEach(workspace => {
+        const cycleRes = await getCyclesByWorkspace(workspace.name)
+        if (cycleRes) {
+          workspace.cycles = cycleRes.result
+          this.setState({
+            workspaces: [...this.state.workspaces, workspace]
+          })
+        }
+      })
+    }
   }
 
   render() {
@@ -29,11 +37,27 @@ class Workspaces extends Component {
         <Nav />
         <h1>HI</h1>
         <ul>
-          {this.state.cycles.map((cycle, index) => {
+          {this.state.workspaceRes.map((workspace, index) => {
             return (
-              <li key={index} className={cycle.current && 'current'}>
-                {cycle.term}
-              </li>
+              <>
+                <h2>{workspace.name}</h2>
+                <Table size="m" hover>
+                  <tr>
+                    <th>Term</th>
+                    <th>Current</th>
+                    <th>Actions</th>
+                  </tr>
+                  {workspace.cycles.map((cycle, index) => {
+                    return (
+                      <tr key={index} className={cycle.current && 'current'}>
+                        <td>{cycle.term}</td>
+                        <td>{cycle.current}</td>
+                        <td>Import things</td>
+                      </tr>
+                    )
+                  })}
+                </Table>
+              </>
             )
           })}
         </ul>
