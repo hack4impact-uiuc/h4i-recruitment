7e91d254faefa660c694954c40ce15303fda36cc
Timothy Ko <timothy.l.ko@gmail.com>
2019-08-20 12:18:24 -0700

    update comment

--- frontend/src/components/common/infoAlert.js
+++ frontend/src/components/common/infoAlert.js
@@ -9,11 +9,12 @@ const turnOff = (toggle, cookieName, cookieAlertOffValue, cookieExpire) => {
 }
 
 /*
- * Info Alert Shows a Bootstrap Alert https://reactstrap.github.io/components/alerts/
+ * Info Alert shows a Bootstrap Alert https://reactstrap.github.io/components/alerts/
  * meant to only show up until the user toggles it off.
  * That state is tracked by using cookies.
- * To create your own alert, provide a cookieName (str) and a cookie value that's created when
- * the user toggles the alert off. Additionally, provide the content JSX through the `content` prop.
+ * To create your own alert, provide a cookieName (str) and a cookieAlertOffValue (str). This cookie
+ * is created when the user toggles the alert off.
+ * Additionally, any children that you put within this component will show up inside the alert.
  * To update an alert (you want a new alert to show up in the same place), change the cookie value
  * to another string value (if the cookie value is equal to the passed prop cookie value
  * doesn't show the alert, else it does)
