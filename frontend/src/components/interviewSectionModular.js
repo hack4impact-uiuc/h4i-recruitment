hack4impact-uiuc/h4i-recruitment

commit 4878fad7fe1a033f0e48adc42d5cd7e7d2e1a374

Code Sanitization (#302)
* Standardize named imports, removed unused Prop types

* Add trailing commas, standardize whitespace between functions

* Move to inline conditional rendering

* Clean up markdown files

* Move to fragments for wrapping component returns

* Clean null returns, use strict equality statemnts

* Undo trailing commas (for this pr)

* Remove unecessary fragment

frontend/src/components/interviewSectionModular.js
@@ -58,6 +58,7 @@ class InterviewSectionModular extends Component {
         </p>
       )
     }
+
     let options = []
     if (this.props.scoreOptions && this.props.textOptions) {
       for (let i = 0; i < this.props.scoreOptions.length; i++) {
@@ -78,42 +79,46 @@ class InterviewSectionModular extends Component {
         options.push({ name: this.props.textOptions[i], value: this.props.textOptions[i] })
       }
     }
+
     return (
       <InterviewSectionCard title={this.props.title}>
-        {this.props.description ? (
+        {this.props.description && (
           <>
             {this.props.description}
             <br /> <br />
           </>
-        ) : null}
+        )}
 
-        {this.props.prompt ? (
+        {this.props.prompt && (
           <>
             {this.props.prompt} <br /> <br />
           </>
-        ) : null}
+        )}
 
-        {this.props.type == 'notes'
+        {this.props.type === 'notes'
           ? null
-          : this.props.type == 'dropdown'
+          : this.props.type === 'dropdown'
           ? this.mapOptionsDropdown(options)
           : this.mapOptionsMultipleChoice(options)}
-        {this.props.type != 'notes' && this.props.notesPrompt ? (
+
+        {this.props.type !== 'notes' && this.props.notesPrompt && (
           <>
             <br />
           </>
-        ) : null}
-        {this.props.notesPrompt || this.props.type == 'notes' ? (
-          <Input
-            style={{ height: '130px' }}
-            value={this.props.notes}
-            className="textarea-input"
-            onChange={this.handleNotesChange}
-            type="textarea"
-            id="time-commitment-explanation"
-            placeholder={this.props.notesPrompt}
-          />
-        ) : null}
+        )}
+
+        {this.props.notesPrompt ||
+          (this.props.type === 'notes' && (
+            <Input
+              style={{ height: '130px' }}
+              value={this.props.notes}
+              className="textarea-input"
+              onChange={this.handleNotesChange}
+              type="textarea"
+              id="time-commitment-explanation"
+              placeholder={this.props.notesPrompt}
+            />
+          ))}
       </InterviewSectionCard>
     )
   }

frontend/src/actions/actionCreators.js
@@ -15,22 +15,28 @@ import {
   SET_VALID_FORMAT
 } from './actionTypes.js'
 import { getCandidates } from '../utils/api'
+
 // Action Creators
 
 export const setRoundRedux = round => ({
   type: SET_ROUND,
   payload: round
 })
+
 export const setSelectedRound = selectedRound => ({
   type: SET_SELECTED_ROUND,
   payload: selectedRound
 })
+
 export const setValidFormat = validFormat => ({
   type: SET_VALID_FORMAT,
   payload: validFormat
 })
+
 export const fetchCandidatesFailure = error => ({ type: FETCH_CANDIDATES_FAILURE, payload: error })
+
 export const fetchCandidatesBegin = () => ({ type: FETCH_CANDIDATES_BEGIN })
+
 export const setStatus = (candidateId, status) => ({
   type: SET_STATUS,
   payload: {
@@ -38,10 +44,12 @@ export const setStatus = (candidateId, status) => ({
     status: status
   }
 })
+
 export const fetchCandidatesSuccess = candidates => ({
   type: FETCH_CANDIDATES_SUCCESS,
   payload: candidates
 })
+
 export const addInterviewCandidate = (candidateId, candidateName) => ({
   type: ADD_INTERVIEW_CANDIDATE,
   payload: {
@@ -49,6 +57,7 @@ export const addInterviewCandidate = (candidateId, candidateName) => ({
     candidateName: candidateName
   }
 })
+
 export const editInterview = interview => ({
   type: EDIT_INTERVIEW,
   payload: {
@@ -79,6 +88,7 @@ export const removeFilter = (category, filter) => ({
     filter: filter
   }
 })
+
 // Function to create action that stores data about new match
 function newMatch(candidate1, candidate2, match_id) {
   return {

frontend/src/components/actionButton.js
@@ -1,4 +1,3 @@
-import { Component } from 'react'
 import Link from 'next/link'
 
 const ActionButton = ({ className, link, text, white, onClick }) => (

frontend/src/components/candidateCard.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import Link from 'next/link'
-import { Card, CardBody, CardTitle, Button, Badge, Row, Col } from 'reactstrap'
+import { Card, CardBody, CardTitle, Badge, Row, Col } from 'reactstrap'
 import Router from 'next/router'
 import { setCandidateStatus } from '../utils/api'
 import { statusEnum } from '../utils/enums'
@@ -15,10 +15,6 @@ const handler = (_id: string) =>
     pathname: '/candidate',
     query: { id: _id }
   })
-
-type Props = {
-  candidate: Array<mixed> // TODO: make this more specific
-}
 
 const mapDispatchToProps = dispatch => {
   return bindActionCreators(
@@ -36,11 +32,13 @@ class CandidateCardComponent extends Component {
       status: this.props.candidate.status
     }
   }
+
   handleChange = e => {
     setCandidateStatus(this.props.candidate._id, e.target.value)
     this.props.setStatus(this.props.candidate._id, e.target.value)
     this.setState({ status: e.target.value })
   }
+
   render() {
     const { candidate } = this.props
     return (
@@ -49,107 +47,83 @@ class CandidateCardComponent extends Component {
           <CardTitle>
             <Row>
               <Col md={12}>
-                {candidate.name ? (
+                {candidate.name && (
                   <Link href={{ pathname: '/candidate', query: { id: candidate._id } }}>
                     <a className="card-title">{candidate.name}</a>
                   </Link>
-                ) : (
-                  <></>
                 )}
               </Col>
             </Row>
             <Row>
               <Col md={12}>
-                {candidate.status && candidate.status === 'Accepted' ? (
+                {candidate.status && candidate.status === 'Accepted' && (
                   // Replaced the span with Badge for easier, more consistent styling
                   <h5>
                     <Badge color="success">Accepted</Badge>
                   </h5>
-                ) : (
-                  <></>
                 )}
 
-                {candidate.status && candidate.status === 'Pending' ? (
+                {candidate.status && candidate.status === 'Pending' && (
                   <h5>
                     <Badge color="warning">Pending</Badge>
                   </h5>
-                ) : (
-                  <></>
                 )}
 
-                {candidate.status && candidate.status === 'Rejected' ? (
+                {candidate.status && candidate.status === 'Rejected' && (
                   <h5>
                     <Badge color="danger">Rejected</Badge>
                   </h5>
-                ) : (
-                  <></>
                 )}
 
-                {candidate.status && candidate.status === 'Interviewing' ? (
+                {candidate.status && candidate.status === 'Interviewing' && (
                   <h5>
                     <Badge color="info">Interviewing</Badge>
                   </h5>
-                ) : (
-                  <></>
                 )}
               </Col>
             </Row>
           </CardTitle>
           <div onClick={e => handler(candidate._id)}>
-            {candidate.major ? (
+            {candidate.major && (
               <p>
                 Major: <span className="highlight">{candidate.major}</span>
               </p>
-            ) : (
-              <></>
             )}
-            {candidate.graduationDate ? (
+            {candidate.graduationDate && (
               <p>
                 <span className="highlight">{candidate.graduationDate}</span>
               </p>
-            ) : (
-              <></>
             )}
 
-            {candidate.role ? (
+            {candidate.role && (
               <p>
                 <span className="highlight">{candidate.role.join(', ')}</span>
               </p>
-            ) : (
-              <></>
             )}
 
-            {candidate.timeCanDevote ? (
+            {candidate.timeCanDevote && (
               <p>
                 Hours: <span className="highlight">{candidate.timeCanDevote}</span>
               </p>
-            ) : (
-              <></>
             )}
 
-            {candidate.year ? (
+            {candidate.year && (
               <p>
                 Year: <span className="highlight">{candidate.year}</span>
               </p>
-            ) : (
-              <></>
             )}
 
-            {candidate.facemashRankings && candidate.facemashRankings.elo ? (
+            {candidate.facemashRankings && candidate.facemashRankings.elo && (
               <p>
                 Facemash Score: <span className="highlight">{candidate.facemashRankings.elo}</span>
               </p>
-            ) : (
-              <></>
             )}
 
-            {candidate.facemashRankings && candidate.facemashRankings.numOfMatches ? (
+            {candidate.facemashRankings && candidate.facemashRankings.numOfMatches && (
               <p>
                 Number of Matches:
                 <span className="highlight">{candidate.facemashRankings.numOfMatches}</span>
               </p>
-            ) : (
-              <></>
             )}
           </div>
           <p>

frontend/src/components/candidateDropdown.js
@@ -11,6 +11,11 @@ type Props = {
   candidates: Array<mixed>
 }
 
+type State = {
+  isClearable: boolean,
+  isSearchable: boolean
+}
+
 const mapStateToProps = state => ({
   loading: state.candidateListPage.candidatesLoading,
   candidateId: state.interview.candidateId,
@@ -26,26 +31,23 @@ const mapDispatchToProps = dispatch => {
   )
 }
 
-type State = {
-  isClearable: boolean,
-  isSearchable: boolean
-}
-
 // this component is used in the interview page for the user to choose candidate
 class CandidateDropdown extends Component<Props, State> {
-  constructor(props) {
-    super(props)
-  }
   state = {
     isClearable: false,
     isSearchable: true,
     selectedOption: null
   }
+
+  constructor(props) {
+    super(props)
+  }
+
   handleChange = selectedOption => {
     this.setState({ selectedOption: selectedOption })
     let chosenCandidate = null
     for (var i = 0; i < this.props.candidates.length; i++) {
-      if (this.props.candidates[i].name == selectedOption.value) {
+      if (this.props.candidates[i].name === selectedOption.value) {
         chosenCandidate = this.props.candidates[i]
       }
     }
@@ -60,8 +62,9 @@ class CandidateDropdown extends Component<Props, State> {
       this.setState({ selectedOption: newSelectedOption })
     }
   }
+
   render() {
-    const { isClearable, isSearchable, selectedOption } = this.state
+    const { selectedOption } = this.state
     const { candidates } = this.props
     var candidate_names = []
     for (var i = 0; i < candidates.length; i++) {
@@ -69,15 +72,14 @@ class CandidateDropdown extends Component<Props, State> {
       let curr_name_label = { value: curr_cand.name.toString(), label: curr_cand.name.toString() }
       candidate_names.push(curr_name_label)
     }
+
     return (
-      <Fragment>
-        <Select
-          defaultValue={candidate_names[0]}
-          options={candidate_names}
-          onChange={this.handleChange}
-          value={selectedOption}
-        />
-      </Fragment>
+      <Select
+        defaultValue={candidate_names[0]}
+        options={candidate_names}
+        onChange={this.handleChange}
+        value={selectedOption}
+      />
     )
   }
 }

frontend/src/components/candidateList.js
@@ -4,6 +4,10 @@ import CandidateCardComponent from './candidateCard'
 import { Col, Form, FormGroup, Label, Input, Row } from 'reactstrap'
 import { connect } from 'react-redux'
 
+type Props = {
+  candidates: Array<mixed>
+}
+
 const CardCol = ({ children, ...rest }) => (
   // This handles the size of each card - lg size 3 causes 4 cards/row
   <Col xs={{ size: 12 }} md={{ size: 6 }} lg={{ size: 3 }} className="mb-3" {...rest}>
@@ -11,13 +15,10 @@ const CardCol = ({ children, ...rest }) => (
   </Col>
 )
 
-type Props = {
-  candidates: Array<mixed>
-}
-
 const mapStateToProps = state => ({
   loading: state.candidateListPage.candidatesLoading
 })
+
 // component that destructs Props - Props would look like this { candidates: {} }
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
 class CandidateListComponent extends Component<Props> {
@@ -27,14 +28,16 @@ class CandidateListComponent extends Component<Props> {
       search: ''
     }
   }
+
   handleChange = event => {
     this.setState({ [event.target.id]: event.target.value })
   }
+
   render() {
     const { search } = this.state
     const { candidates } = this.props
     return (
-      <div>
+      <>
         <Row>
           <Col sm="12">
             <Form onSubmit={this.handleSubmit}>
@@ -68,10 +71,10 @@ class CandidateListComponent extends Component<Props> {
                 )
               })
           ) : (
-            <div>Loading</div>
+            <>Loading</>
           )}
         </Row>
-      </div>
+      </>
     )
   }
 }

frontend/src/components/candidateStatus.js
@@ -1,34 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Badge } from 'reactstrap'
 import { statusEnum } from '../utils/enums'
 
 // a Bootstrap badge that changes colors depending on the status
-class CandidateStatus extends React.Component {
+class CandidateStatus extends Component {
   render() {
     const { status } = this.props
     return (
       <>
-        {status && status === statusEnum.ACCEPTED ? (
-          // Replaced the span with Badge for easier, more consistent styling
-          <Badge color="success">Accepted</Badge>
-        ) : (
-          <></>
-        )}
-
-        {status && status === statusEnum.PENDING ? <Badge color="warning">Pending</Badge> : <></>}
-
-        {status && status === statusEnum.REJECTED ? <Badge color="danger">Rejected</Badge> : <></>}
-
-        {status && status === statusEnum.INTERVIEWING ? (
-          <Badge color="info">Interviewing</Badge>
-        ) : (
-          <></>
-        )}
-        {status && status === statusEnum.INVALID ? <Badge color="secondary">Invalid</Badge> : <></>}
-        {status && status === statusEnum.DONE_INTERVIEWING ? (
+        {status && status === statusEnum.ACCEPTED && <Badge color="success">Accepted</Badge>}
+        {status && status === statusEnum.PENDING && <Badge color="warning">Pending</Badge>}
+        {status && status === statusEnum.REJECTED && <Badge color="danger">Rejected</Badge>}
+        {status && status === statusEnum.INTERVIEWING && <Badge color="info">Interviewing</Badge>}
+        {status && status === statusEnum.INVALID && <Badge color="secondary">Invalid</Badge>}
+        {status && status === statusEnum.DONE_INTERVIEWING && (
           <Badge color="primary">Done Interviewing</Badge>
-        ) : (
-          <></>
         )}
       </>
     )

frontend/src/components/candidates/candidateBox.js
@@ -3,7 +3,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { Badge, Row, Col } from 'reactstrap'
+import { Row, Col } from 'reactstrap'
 import { setCandidateStatus } from '../../utils/api'
 import { setStatus } from '../../actions/actionCreators'
 import ErrorMessage from '../errorMessage'
@@ -11,11 +11,6 @@ import CandidateStatus from '../candidateStatus'
 import CandidateLinks from '../candidateLinks'
 import ChangeStatus from '../changeStatus'
 import { avgInterviewScore, interviewGetCategorySection } from '../../utils/core'
-
-type Props = {
-  candidate: {},
-  hideStatus?: boolean
-}
 
 const mapDispatchToProps = dispatch => {
   return bindActionCreators(
@@ -35,11 +30,13 @@ class CandidateBox extends Component {
       avgInterviewScore: null
     }
   }
+
   handleChange = e => {
     setCandidateStatus(this.props.candidate._id, e.target.value)
     this.props.setStatus(this.props.candidate._id, e.target.value)
     this.setState({ status: e.target.value })
   }
+
   async componentDidMount() {
     const { interviews } = this.props.candidate
     this.setState({
@@ -47,12 +44,14 @@ class CandidateBox extends Component {
       avgInterviewScore: avgInterviewScore(interviews)
     })
   }
+
   render() {
     // includes null
     if (this.props.candidate == undefined) {
       return <ErrorMessage message="User doesn&#39;t exist" />
     }
     const { candidate } = this.props
+
     return (
       <div className="candidate-box-wrapper">
         <Row>

commit e7eea745cfa649417cafedcb119072227c2a309b

Fix interview rendering due to regression in #302

frontend/src/components/interviewSectionModular.js
@@ -79,7 +79,7 @@ class InterviewSectionModular extends Component {
         options.push({ name: this.props.textOptions[i], value: this.props.textOptions[i] })
       }
     }
-
+    console.log('notes', this.props.type, this.props.notesPrompt)
     return (
       <InterviewSectionCard title={this.props.title}>
         {this.props.description && (
@@ -107,18 +107,17 @@ class InterviewSectionModular extends Component {
           </>
         )}
 
-        {this.props.notesPrompt ||
-          (this.props.type === 'notes' && (
-            <Input
-              style={{ height: '130px' }}
-              value={this.props.notes}
-              className="textarea-input"
-              onChange={this.handleNotesChange}
-              type="textarea"
-              id="time-commitment-explanation"
-              placeholder={this.props.notesPrompt}
-            />
-          ))}
+        {(this.props.notesPrompt || this.props.type === 'notes') && (
+          <Input
+            style={{ height: '130px' }}
+            value={this.props.notes}
+            className="textarea-input"
+            onChange={this.handleNotesChange}
+            type="textarea"
+            id="time-commitment-explanation"
+            placeholder={this.props.notesPrompt}
+          />
+        )}
       </InterviewSectionCard>
     )
   }