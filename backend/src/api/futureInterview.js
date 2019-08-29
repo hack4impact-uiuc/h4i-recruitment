ea728e2ccae9177e4dacdfd4ff7ac8ae9fe88056
Josh Byster <8664074+josh-byster@users.noreply.github.com>
2019-08-29 14:50:20 -0500

    Automatic Interview Scheduling (#315)
    * Correct serialization of excel file
    
    * Got basic mapping from name to available dates
    
    * Parsing of doodle
    
    * Addition of schedule basic python script
    
    * Added interviewer candidate boxes
    
    * Add persistence to DB
    
    * Removed mock data
    
    * Button validation
    
    * Prettier
    
    * Promisified file uploader
    
    * Conformed to api request
    
    * Got population of FE
    
    * Added interviewAvailability model
    
    * Added nice loading on schedule retrieval
    
    * switch out apiary endpoint to real scheduler endpoint
    
    * completed integration with lambda
    
    * Added delete schedule button
    
    * Prettier format
    
    * Address PR changes
    
    * Added error handling on FE
    
    * Updated upon each API call
    
    * Prettier
    
    * Added delete confirmation
    
    * PR fixups
    
    * Minor fix on response code
    
    * Upload wildcarding
    
    * Added tests
    
    * Lint fixes

--- backend/src/api/futureInterview.js
+++ backend/src/api/futureInterview.js
@@ -1,71 +1,28 @@
 const express = require('express')
-var mongodb = require('mongodb')
+const mongodb = require('mongodb')
+const XLSX = require('xlsx')
+const fetch = require('isomorphic-unfetch')
+const moment = require('moment')
 const csv = require('csv-array')
 const { errorWrap, leadsOnly } = require('../middleware')
-const { FutureInterview } = require('../models')
+const { FutureInterview, InterviewAvailability } = require('../models')
 const keyPath =
   process.env.NODE_ENV === 'test' ? '../../tests/artifacts/test-keys.json' : process.env.KEY_JSON
+const schedulerUrl = process.env.SCHEDULER_URL
+const schedulerApiKey = process.env.SCHEDULER_API_KEY
 const keyData = require(keyPath)
-
 const router = express.Router()
 
-router.post(
-  '/upload',
-  [leadsOnly],
-  errorWrap(async (req, res) => {
-    const data = req.body['schedule']
-    let response = 'Schedule Adding Failed'
-    let code = 404
-
-    // TODO: parse schedule
-    var arr = data.split('\n').map(function(e) {
-      return e.split(',')
-    })
-    var date = arr[0][0]
-    var headers = arr[0]
-
-    for (var i = 1; i < arr.length; i++) {
-      for (var j = 1; j < arr[i].length; j++) {
-        if (arr[i][j] === '') continue
-
-        var interview = arr[i][j]
-        var interviewers = interview
-          .split(':')[1]
-          .split(';')[0]
-          .split('&')
-          .map(s => s.trim())
-        var interviewees = interview
-          .split(':')[2]
-          .split('&')
-          .map(s => s.trim())
-
-        let newInterview = new FutureInterview({
-          candidates: interviewees,
-          interviewers: interviewers,
-          room: arr[0][j],
-          date: date,
-          time: arr[i][0]
-        })
-        const res = await newInterview.save()
-      }
-    }
-
-    response = 'Schedule Added Sucessfully'
-    code = 200
-    res.json({
-      code,
-      message: response,
-      result: data,
-      success: true
-    })
-  })
-)
+const listType = {
+  INTERVIEWERS: 'INTERVIEWERS',
+  CANDIDATES: 'CANDIDATES'
+}
 
 // This endpoint is an example
 router.post(
   '/populateTest',
   errorWrap(async (req, res) => {
-    var newInterview = new FutureInterview({
+    let newInterview = new FutureInterview({
       candidates: ['Steve Jobs'],
       interviewers: ['Tim Ko'],
       room: 'Room A',
@@ -93,35 +50,159 @@ router.post(
   })
 )
 
+router.post(
+  '/generateSchedules',
+  errorWrap(async (req, res) => {
+    const interviewerAvail = await InterviewAvailability.findOneAndDelete({
+      type: listType.INTERVIEWERS
+    })
+    const candidateAvail = await InterviewAvailability.findOneAndDelete({
+      type: listType.CANDIDATES
+    })
+    const result = await fetch(schedulerUrl, {
+      body: JSON.stringify({
+        interviewerAvailabilities: { times: interviewerAvail.availabilities },
+        candidateAvailabilities: { times: candidateAvail.availabilities },
+        timeSlots: interviewerAvail.timeSlots,
+        interviewDuration: interviewerAvail.interviewDuration
+      }),
+      headers: {
+        'x-api-key': schedulerApiKey,
+        'content-type': 'application/json'
+      },
+      method: 'POST'
+    })
+    json = await result.json()
+    insertions = json.scheduledInterviews.map(value => {
+      start_date = moment(new Date(value.startTime))
+      return FutureInterview({
+        candidates: [value.name.replace('_', ' ')],
+        interviewers: value.interviewers.map(x => x.replace('_', ' ')),
+        date: start_date.format('MM/DD/YYYY'),
+        time: start_date.format('hh:mmA'),
+        room: ''
+      }).save()
+    })
+    await Promise.all(insertions)
+    res.json({
+      code: 200,
+      message: 'Populated.',
+      result: {},
+      success: true
+    })
+  })
+)
+
+router.post(
+  '/upload:type',
+  errorWrap(async (req, res) => {
+    const workbook = XLSX.read(req.body['data'], { type: 'binary' })
+    const sheetName = workbook.SheetNames
+    const sheet = workbook['Sheets'][sheetName]
+    const arr = XLSX.utils.sheet_to_json(sheet, { header: 1, raw: false, defval: null })
+    unmergeColumns(arr)
+    let timeObj = getTimesFromArray(arr)
+    try {
+      await uploadInterviewAvailability(timeObj, req.params.type.toLowerCase() === 'interviewers')
+      res.json({
+        code: 200,
+        message: 'Populated.',
+        result: {},
+        success: true
+      })
+    } catch (e) {
+      res.json({
+        code: 400,
+        message: 'Error.',
+        result: {},
+        success: false
+      })
+    }
+  })
+)
+
+const uploadInterviewAvailability = async (obj, isInterviewerList) => {
+  let availability = new InterviewAvailability({
+    type: isInterviewerList ? listType.INTERVIEWERS : listType.CANDIDATES,
+    interviewDuration: 30,
+    availabilities: obj.times,
+    timeSlots: obj.allTimes
+  })
+  await availability.save()
+}
+
+// since the first 3 rows have merged cells for the month and date,
+// we want to "unmerge" so there are no null cells in the month and date rows
+const unmergeColumns = arr => {
+  let valueToFill
+  const NUM_DATE_ROWS = 2
+  for (let i = 0; i < NUM_DATE_ROWS; i++) {
+    for (let j = 1; j < arr[i].length; j++) {
+      if (arr[i][j] !== null) {
+        valueToFill = arr[i][j]
+      } else {
+        arr[i][j] = valueToFill
+      }
+    }
+  }
+}
+
+const getTimesFromArray = arr => {
+  let summaryDict = {}
+  let timeArray = []
+  // Index of first row in which candidates / interviewers have their times listed
+  const STARTING_AVAILABILITY_ROW = 3
+  for (let row = STARTING_AVAILABILITY_ROW; row < arr.length - 1; row++) {
+    let personObj = {}
+    personObj['name'] = arr[row][0]
+    personObj['availableTimes'] = []
+    for (let col = 1; col < arr[row].length; col++) {
+      if (arr[row][col] === 'OK') {
+        // grab the date from the first few rows, concatenate and parse that date
+        let datetimeStr = `${arr[0][col]} ${arr[1][col]} ${arr[2][col]}`
+        personObj['availableTimes'].push(moment(datetimeStr, 'MMMM YYYY ddd DD hh:mm a').toDate())
+      }
+    }
+    timeArray.push(personObj)
+  }
+  summaryDict['times'] = timeArray
+  summaryDict['allTimes'] = []
+  for (let col = 1; col < arr[0].length; col++) {
+    let datetimeStr = `${arr[0][col]} ${arr[1][col]} ${arr[2][col]}`
+    summaryDict['allTimes'].push(moment(datetimeStr, 'MMMM YYYY ddd DD hh:mm a').toDate())
+  }
+  return summaryDict
+}
+
 router.get(
   '/',
   [leadsOnly],
   errorWrap(async (req, res) => {
-    const futureInterviews = await FutureInterview.find()
+    const futureInterviews = await FutureInterview.find().sort({ _id: 1 })
     let interviews = []
-    let code = 500
-    // TODO: do stuff
-    // retrieve list of FutureInterviews and return them
-
-    /*
-    for (var idx = 0; idx < futureInterviews.length; idx++) {
-      if (futureInterviews[idx].date > //!!current date - how do this?!!//) {
-        interviews.push(futureInterviews[idx])
-      }
-    }
-    */
-
-    // for now we'll do:
     interviews = futureInterviews
-    code = 200
     res.json({
-      code,
-      message: 'This endpoint must be implemented',
+      code: 200,
+      message: 'Retrieved interview schedules',
       result: { interviews },
       success: true
     })
   })
 )
 
+router.delete(
+  '/',
+  [leadsOnly],
+  errorWrap(async (req, res) => {
+    const result = await FutureInterview.deleteMany({})
+    console.log(result)
+    res.json({
+      code: 200,
+      message: 'Deleted interview schedules',
+      result: {},
+      success: true
+    })
+  })
+)
 // END
 module.exports = router
