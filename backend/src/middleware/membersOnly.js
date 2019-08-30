c3deea059c2b83a19dbb86688aadea0d98519e19
Angad Garg <angad.garg3@gmail.com>
2019-08-30 01:46:37 -0500

    bla

--- backend/src/middleware/membersOnly.js
+++ backend/src/middleware/membersOnly.js
@@ -3,7 +3,9 @@
  */
 const membersOnly = (req, res, next) => {
   if (!req._is_member) {
-    const msg = `The key '${req._key}' doesn't have the correct privileges. Please contact the admin.`
+    const msg = `The key '${
+      req._key
+    }' doesn't have the correct privileges. Please contact the admin.`
     console.error(msg)
     res.status(403).json({
       code: 403,
