5d6f3b845c568de72dc16c3d06a64f4cb52969ed
Angad Garg <angad.garg3@gmail.com>
2019-08-30 13:03:47 -0500

    attempt to fix tests

--- backend/tests/mongo_utils.js
+++ backend/tests/mongo_utils.js
@@ -1,55 +1,67 @@
 const mongoose = require('mongoose')
 const { MongoMemoryServer } = require('mongodb-memory-server')
-const {User} = require('../src/models')
+const { User } = require('../src/models')
 
 let mongoServer
 
 before(async done => {
   mongoServer = new MongoMemoryServer()
-  mongoServer
-    .getConnectionString()
-    .then(mongoUri => {
-      return mongoose.connect(
-        mongoUri,
-        {},
-        err => {
-          if (err) {
-            done(err)
-          }
-        }
-      )
-    })
-    .then(() => done())
+  // mongoServer
+  //   .getConnectionString()
+  //   .then(mongoUri => {
+  //     return mongoose.connect(
+  //       mongoUri,
+  //       {},
+  //       err => {
+  //         if (err) {
+  //           done(err)
+  //         }
+  //       }
+  //     )
+  //   })
+  //   .then(() => done())
+  const mongoUri = await mongoServer.getConnectionString()
+  await mongoose.connect(
+    mongoUri,
+    {},
+    err => {
+      if (err) {
+        done(err)
+      }
+    }
+  )
 
   await User.insertMany([
     {
-      firstName: "Member",
-      lastName: "Test",
-      userId: "member",
-      email: "m@t.com",
-      key: "member",
-      role: "Member",
-      workspaceId: "abc"
+      firstName: 'Member',
+      lastName: 'Test',
+      userId: 'member',
+      email: 'm@t.com',
+      key: 'member',
+      role: 'Member',
+      workspaceId: 'abc'
     },
     {
-      firstName: "Lead",
-      lastName: "Test",
-      userId: "lead",
-      email: "l@t.com",
-      key: "lead",
-      role: "Lead",
-      workspaceId: "abc"
+      firstName: 'Lead',
+      lastName: 'Test',
+      userId: 'lead',
+      email: 'l@t.com',
+      key: 'lead',
+      role: 'Lead',
+      workspaceId: 'abc'
     },
     {
-      firstName: "Director",
-      lastName: "Test",
-      userId: "director",
-      email: "d@t.com",
-      key: "director",
-      role: "Director",
-      workspaceId: "abc"
+      firstName: 'Director',
+      lastName: 'Test',
+      userId: 'director',
+      email: 'd@t.com',
+      key: 'director',
+      role: 'Director',
+      workspaceId: 'abc'
     }
   ])
+
+  done()
 })
 
 after(() => {
