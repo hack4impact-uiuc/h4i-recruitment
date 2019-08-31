1025e6163ed0bf68862e2dddbfa8a3411e299e32
Angad Garg <angad.garg3@gmail.com>
2019-08-31 11:11:54 -0500

    fix?

--- backend/tests/mongo_utils.js
+++ backend/tests/mongo_utils.js
@@ -6,16 +6,16 @@ let mongoServer
 
 before(async () => {
   mongoServer = new MongoMemoryServer()
-  // mongoServer
-  //   .getConnectionString()
-  //   .then(mongoUri => {
-  //     return mongoose.connect(mongoUri, {}, err => {
-  //       if (err) {
-  //         done(err)
-  //       }
-  //     })
-  //   })
-  //   .then(() => done())
+  mongoServer
+    .getConnectionString()
+    .then(mongoUri => {
+      return mongoose.connect(mongoUri, {}, err => {
+        if (err) {
+          done(err)
+        }
+      })
+    })
+    .then(() => done())
   const mongoUri = await mongoServer.getConnectionString()
   await mongoose.connect(
     mongoUri,
