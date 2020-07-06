4df215b291422bcb1f81ab8dc60c9f3b8df98bca
Alice Fang <alicesf2@illinois.edu>
2020-07-06 00:05:45 -0500

    dockerized mongo

--- backend/recreate_db.sh
+++ backend/recreate_db.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-docker_ps_out=$(docker ps | grep 'mongo')
-read -ra docker_args <<< "$docker_ps_out"
-docker_id=${docker_args[0]}
-docker stop $docker_id
-echo y | docker system prune
-echo y | docker volume prune
-docker run -v mongodbvol:/data/db -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=secret -p 27017:27017 -d mongo
-echo "recreating DB and populating..."
-sleep 4
 dotenv yarn populatedb
