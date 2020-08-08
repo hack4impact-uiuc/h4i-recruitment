0ff315159bcf53d9d810073861fa064b1558565e
Brandon Wang <sourbrandon@gmail.com>
2020-08-07 23:06:39 -0700

    remove unnecessary console log

--- frontend/src/utils/apiHelpers.js
+++ frontend/src/utils/apiHelpers.js
@@ -2,6 +2,4 @@ import urljoin from 'url-join'
 
 const API_URI = process.env.REACT_APP_API_URI ? process.env.REACT_APP_API_URI : '/'
 
-console.log(process.env.REACT_APP_API_URI)
-
 export const getURLForEndpoint = endpoint => urljoin(API_URI, endpoint)
