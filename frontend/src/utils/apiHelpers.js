b267de6143e37589ba7ff491a4bddfd25604340e
Brandon Wang <sourbrandon@gmail.com>
2020-08-14 15:15:27 -0700

    fix api helpers

--- frontend/src/utils/apiHelpers.js
+++ frontend/src/utils/apiHelpers.js
@@ -3,4 +3,4 @@
 const API_URI = process.env.REACT_APP_API_URI ? process.env.REACT_APP_API_URI : '/'
 
 // export const getURLForEndpoint = endpoint => urljoin(API_URI, endpoint)
-export const getURLForEndpoint = endpoint => `http://localhost:8080/${endpoint}`
+export const getURLForEndpoint = endpoint => `${API_URI}${endpoint}`
